38e8b07f78f04dab4210b501f605fb35d480653a
Philippe Bellanger <pbellanger@hotmail.com>
2015-12-25 15:24:58 -0500

    Updated build property to match LTE-compatible radio band

--- occam.mk
+++ occam.mk
@@ -99,7 +99,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 	keyguard.no_require_sim=true \
 	af.fast_track_multiplier=1 \
 	ro.build.expect.bootloader=MAKOZ30f \
-	ro.build.expect.baseband=M9615A-CEFWMAZM-2.0.1700.48 \
+	ro.build.expect.baseband=M9615A-CEFWMAZM-2.0.1700.27 \
 
 # reduce system image size
 PRODUCT_AAPT_CONFIG := normal

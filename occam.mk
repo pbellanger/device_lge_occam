99672c0183bc12deb4cbdf4aa8844091d4d4f470
Philippe Bellanger <pbellanger@hotmail.com>
2018-02-05 21:13:03 -0500

    Removed product overrides to minimize differences with Unlegacy ROM

--- occam.mk
+++ occam.mk
@@ -23,19 +23,9 @@ GAPPS_FORCE_PIXEL_LAUNCHER := true
 PRODUCT_PACKAGE_OVERLAYS := device/lge/occam/overlay
 
 PRODUCT_PROPERTY_OVERRIDES := \
-	ro.com.android.dateformat=MM-dd-yyyy \
-	ro.com.android.dataroaming=false \
-	net.bt.name=Nexus4 \
 	ro.config.ringtone=Zen.ogg \
 	ro.config.notification_sound=Chime.ogg \
 	ro.config.alarm_alert=Flow.ogg \
-	ro.telephony.default_network=3 \
-	ro.ril.def.preferred.network=3 \
-	ro.com.android.mobiledata=true \
-
-# Nexus 4 has no gesture sensor
-PRODUCT_PROPERTY_OVERRIDES += \
-	gesture.disable_camera_launch=1 \
 
 # remove unecessary google packages to reduce system image
 # Nexus 4 does not support VR
@@ -91,27 +81,6 @@ ifeq ($(TARGET_BUILD_VARIANT),user)
 	ro.adb.secure=1 \
 
 endif
-
-PRODUCT_PROPERTY_OVERRIDES += \
-	ro.hwui.texture_cache_size=48 \
-	ro.hwui.layer_cache_size=32 \
-	ro.hwui.r_buffer_cache_size=4 \
-	ro.hwui.path_cache_size=24 \
-	ro.hwui.gradient_cache_size=1 \
-	ro.hwui.drop_shadow_cache_size=5 \
-	ro.hwui.texture_cache_flushrate=0.5 \
-	ro.hwui.text_small_cache_width=1024 \
-	ro.hwui.text_small_cache_height=1024 \
-	ro.hwui.text_large_cache_width=2048 \
-	ro.hwui.text_large_cache_height=1024 \
-
-PRODUCT_PROPERTY_OVERRIDES += \
-	keyguard.no_require_sim=true \
-	af.fast_track_multiplier=1 \
-	ro.build.expect.bootloader=MAKOZ30f \
-	ro.build.expect.baseband=M9615A-CEFWMAZM-2.0.1700.27 \
-        ro.product.first_api_level=17 \
-        media.stagefright.legacyencoder=1
 
 # reduce system image size
 PRODUCT_AAPT_CONFIG := normal
